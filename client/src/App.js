718926e63f291b62ca3272a5d888c1eb9010056c
hashimkalam <hashim.20211291@iit.ac.lk>
2024-01-26 15:29:01 +0530

    downlaod page - notfully responsive

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import ForgotPassword from "./components/ForgotPassword";
 import Register from "./components/Register";
+import Download from "./components/Download";
 
 function App() {
   return (
@@ -33,7 +34,14 @@ function App() {
               </div>
             }
           />
-
+          <Route
+            path="/download"
+            element={
+              <div className="bg-[#090E34]">
+                <Download />
+              </div>
+            }
+          />
           <Route
             path="/"
             element={
