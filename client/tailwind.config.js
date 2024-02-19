ed7fee54085ff494c6ccf4af7864a50d9b9e421d
salmanfaraj06 <salman.faraj06@gmail.com>
2024-02-19 18:41:34 +0530

    made footer resposnsive

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      colors: {
+        'custom-blue': '#090e34',
+      },
       screens: {
         xxs: "320px",
       },
