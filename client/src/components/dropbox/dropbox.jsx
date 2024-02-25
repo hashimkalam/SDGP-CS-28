9f02e0ea49b6b34f67ed847eba2acecf6a6dcad6
MhMdRiyazath <mohamedriyazath.prof@gmail.com>
2024-02-25 17:05:18 +0530

    img upload to storage

--- client/src/components/dropbox/dropbox.jsx
+++ client/src/components/dropbox/dropbox.jsx
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {  useSelector } from "react-redux";
+import { storage } from "../../firebase"
+import { ref, uploadBytes } from 'firebase/storage'
+import { v4 } from "uuid"
 
 function dropbox() {
-    const handleFiles = (e) => {
+    const currentUser = useSelector((state) => state.user.currentUser);
+    const id = currentUser.user._id;
+
+
+    const [img, setImg] = useState(``)
+
+    const uploadData = () => {
+        const imgRef = ref(storage, `arch_files/${id}/floorplan_${v4()}.png`)
+        uploadBytes(imgRef, img);
+
+    }
+
+    const handleOnChange = (e) => {
+        e.preventDefault();
+        setImg(e.target.files[0])
         console.log("selected file", e.target.files);
+        uploadData();
     }
 
 
     const handleDrop = (e) => {
+        setImg(e.dataTransfer.files[0])
         e.stopPropagation();
         e.preventDefault();
         console.log("drag and drop", e.dataTransfer.files);
+        uploadData();
     }
 
     const handledragover = (e) => {
@@ -30,26 +51,26 @@ function dropbox() {
         <>
             <label
                 html-for="dropzone-file"
-                class="flex flex-col items-center justify-center w-full h-full border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+                className="flex flex-col items-center justify-center w-full h-full border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
 
                 onDragEnter={handleDragEnter}
                 onDragLeave={handleDragLeave}
                 onDragOver={handledragover}
                 onDrop={handleDrop}
             >
-                <div class="flex flex-col items-center justify-center pt-5 pb-6">
-                    <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                    <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
                     </svg>
-                    <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
-                    <p class="text-xs text-gray-500 dark:text-gray-400">PNG</p>
+                    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+                    <p className="text-xs text-gray-500 dark:text-gray-400">PNG</p>
                 </div>
 
                 <input
                     id="dropzone-file"
                     type="file"
-                    class="hidden"
-                    onChange={handleFiles}
+                    className="hidden"
+                    onChange={handleOnChange}
                 />
 
             </label>
